692cf5b28aaf23fc106cf54ede6ef6e54e59d741
sebdotv <sebdotv@gmail.com>
2016-11-22 23:30:33 +0100

    cvs export

--- visualvm-viewer/src/main/scala/viewer/ViewerApp.scala
+++ visualvm-viewer/src/main/scala/viewer/ViewerApp.scala
@@ -12,6 +12,13 @@ import javax.swing.JFrame
 import javax.swing.JTree
 import scalaswingcontrib.tree.Tree
 import scalaswingcontrib.tree.TreeModel
+import java.io.FileWriter
+import java.io.BufferedWriter
+import java.io.FileOutputStream
+import java.io.BufferedOutputStream
+import java.io.OutputStreamWriter
+import java.nio.charset.StandardCharsets
+import java.io.Writer
 
 object ViewerApp extends App {
   val VIEW = CPUResultsSnapshot.METHOD_LEVEL_VIEW
@@ -71,8 +78,27 @@ object ViewerApp extends App {
 
     }
 
+  val mergedThreads = MergedThreadsNode(threads)
+
+  val csv = new File("merged.csv")
+  val w = new OutputStreamWriter(new BufferedOutputStream(new FileOutputStream(csv)), StandardCharsets.UTF_8)
+  mergedThreads.children.foreach(writeCsv(Nil, w, _))
+  w.close
+
+  def writeCsv(stack: Seq[String], w: Writer, node: MethodNodeTrait) {
+    val stack2 = stack :+ (if (node.selfTime) "self time" else node.method.className + "." + node.method.name)
+    if (node.children.isEmpty) {
+      w.write(stack2.mkString("-"))
+      w.write(",")
+      w.write(node.times.totalTime0.toString)
+      w.write("\n")
+    } else {
+      node.children.foreach(writeCsv(stack2, w, _))
+    }
+  }
+
   val tree = new Tree[Node] {
-    model = TreeModel[Node](RootNode(List( ThreadsNode(threads), MergedThreadsNode(threads))))(_.children)
+    model = TreeModel[Node](RootNode(List(ThreadsNode(threads), mergedThreads)))(_.children)
     renderer = Tree.Renderer(_.value)
   }
 
